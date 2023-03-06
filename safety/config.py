cc29177105caf873bf6665636409cef9d81bf35e
Ghost <112991104+CrymslavekCodes@users.noreply.github.com>
2023-03-06 18:23:14 +0000

    PLEASSE

---
+++ safety/config.py
@@ -0,0 +1,45 @@
+###############################################
+#           Template made by Person0z         #
+#          https://github.com/Person0z        #
+#           Copyright© Person0z, 2022         #
+#           Do Not Remove This Header         #
+###############################################
+
+# Imports
+import disnake
+
+# Discord Token
+
+
+# Version
+version = '1.5.7'
+
+# Your Discord Server ID Will Go Here 
+guild = '897569767320254926'
+
+# The Prefix You Want For Your Discord Bot
+prefix = 'ch.'
+
+# Bot Status
+activity = ["/help", "discord.py", "With Python", "Made by Person0z", "v1.5.7"]
+
+# Colors
+Success = disnake.Color.green
+Error = disnake.Color.red
+Random = disnake.Color.random
+
+# Owner ID
+owner_ids = [550345850724024341, 7114331936469811611] # You can add more owner ids by adding a comma and the id
+
+# Welcomes & Goodbyes Channel ID
+welcome_channel = 955533754787594280
+
+# Logging Channel ID
+logs = [9291158017877156424] # You can add more channels by doing this: [channel_id, channel_id, channel_id]
+
+# Roles to give users when the reach a certain level
+level_roles = {
+    5: 1082007545998651432,  # Role ID for level 5
+    10: 1081007826752778300,  # Role ID for level 10
+    15: 1081008035515871252,  # Role ID for level 15
+    }
